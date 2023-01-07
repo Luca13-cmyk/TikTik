1db1b994cfcfb020fed0ac78661bdf1e42500015
Luca Negrette <lucanegrette1303@gmail.com>
2023-01-07 15:24:24 -0300

    07/01/2023

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,9 +11,27 @@ import { createOrGetUser } from "../utils";
 
 import useAuthStore from "../store/authStore";
 import user from "../sanity-backend/schemas/user";
+import { IUser } from "../types";
+import React, { useState } from "react";
 
 const Navbar = () => {
-  const { userProfile, addUser, removeUser } = useAuthStore();
+  const {
+    userProfile,
+    addUser,
+    removeUser,
+  }: { userProfile: any; addUser: any; removeUser: any } = useAuthStore();
+
+  const [searchValue, setSearchValue] = useState("");
+
+  const router = useRouter();
+
+  const handleSearch = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (searchValue) {
+      router.push(`/search/${searchValue}`);
+    }
+  };
 
   return (
     <div
@@ -30,7 +48,29 @@ const Navbar = () => {
           />
         </div>
       </Link>
-      <div>SEARCH</div>
+      <div className="relative hidden md:block">
+        <form
+          onSubmit={handleSearch}
+          className="absolute md:static top-10 -left-20 bg-white"
+        >
+          <input
+            type="text"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            placeholder="Search accounts and videos"
+            className="bg-primary p-3 md:text-md font-medium border-2 border-gray-100  
+            focus:outline-none focus:border-2 focus:border-gray-300 w-[300px] md:w-[350px] 
+            rounded-full md:top-0 
+            "
+          />
+          <button
+            onClick={handleSearch}
+            className="absolute md:right-5 right-6 top-4 border-l-2 border-gray-300 pl-4 text-2xl text-gray-400"
+          >
+            <BiSearch />
+          </button>
+        </form>
+      </div>
       <div>
         {userProfile ? (
           <div className="flex gap-5 md:gap-10">
